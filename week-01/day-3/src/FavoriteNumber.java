e64afe76504e8a07e7a8e9111eb8a2349db2e7a1
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 14:53:22 +0100

    9th task

---
+++ week-01/day-3/src/FavoriteNumber.java
@@ -0,0 +1,9 @@
+public class FavoriteNumber
+{
+    public static void main(String[] args)
+    {
+        int favoriteNumber = 7;
+
+        System.out.println("My favorite number is " + favoriteNumber + ".");
+    }
+}
