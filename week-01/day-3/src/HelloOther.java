0a232127b16057c6b2c694aafaf847078817ac27
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 12:38:19 +0100

    3rd task

---
+++ week-01/day-3/src/HelloOther.java
@@ -0,0 +1,9 @@
+public class HelloOther
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Peti!");
+        System.out.println("Hello Kiki!");
+        System.out.println("Hello Adél!");
+    }
+}
