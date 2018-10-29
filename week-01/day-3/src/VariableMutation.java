41ab4e02c4b2d1192e0435e654e0f646b16acd78
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 16:57:15 +0100

    12th task

---
+++ week-01/day-3/src/VariableMutation.java
@@ -0,0 +1,42 @@
+public class VariableMutation
+{
+    public static void main(String[] args)
+    {
+        int a = 3;
+        System.out.println(a + 10);
+
+        int b =100;
+        System.out.println(b - 7);
+
+        int c = 44;
+        System.out.println(c * 2);
+
+        int d =125;
+        System.out.println(d / 5);
+
+        int e = 8;
+        System.out.println(e * e * e);
+
+        int f1 = 123;
+        int f2 = 345;
+        if (f1 > f2)
+        {
+            System.out.println(f1 + " is bigger than " + f2);
+        }
+            else
+                {
+                    System.out.println(f1 + " is smaller than " + f2 + ".");
+                }
+
+        int g1 = 350;
+        int g2 = 200;
+        if ((g2 * 2) > g1)
+        {
+            System.out.println(g2 + " *2 is bigger than " + g1 + ".");
+        }
+
+        int h = 135798745;
+        if ()
+
+    }
+}
