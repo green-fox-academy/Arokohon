e50de58a71a0c6c47a9c1fd264a143b27b6fe5b0
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 15:55:11 +0100

    11th task

---
+++ week-01/day-3/src/DefineBasicInfo.java
@@ -0,0 +1,24 @@
+public class DefineBasicInfo
+{
+    public static void main(String[] args)
+    {
+        String a = "Tamás Szőke";
+        int b = 30;
+        double c = 1.75;
+        boolean married = false;
+
+        System.out.println("My name is " + a + ".");
+        System.out.println("I'm " + b + " years old.");
+        System.out.println("I'm " + c + " m height.");
+        if (married == true){
+            System.out.println("I'm married.");
+        }   else {
+            System.out.println("I'm single. :(");
+        }
+
+
+
+
+
+    }
+}
