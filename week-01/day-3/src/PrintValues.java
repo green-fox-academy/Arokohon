d9c77ebac901c5f77956e4372d91a8c2ebe7c040
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 12:44:21 +0100

    4th task

---
+++ week-01/day-3/src/PrintValues.java
@@ -0,0 +1,9 @@
+public class PrintValues
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("42");
+        System.out.println("3,141592");
+    }
+}
