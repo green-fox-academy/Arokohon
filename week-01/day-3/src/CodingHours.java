a0faa857c0038185b20f7eccee9b4fb4ce312b0b
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 14:47:37 +0100

    8th task

---
+++ week-01/day-3/src/CodingHours.java
@@ -0,0 +1,11 @@
+public class CodingHours
+{
+    public static void main(String[] args)
+    {
+        int a = 6;
+        int b = 17;
+        System.out.println((b * 5) * a);
+
+        System.out.println((((((double)b * 5) * (double)a) / ((double)b * 52)) * 100) + "%");
+    }
+}
