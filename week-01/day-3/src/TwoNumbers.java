80c17b4f55212765204a5d2b7f020b1d6c8ac83e
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 14:32:06 +0100

    7th task

---
+++ week-01/day-3/src/TwoNumbers.java
@@ -0,0 +1,22 @@
+public class TwoNumbers
+{
+    public static void main(String[] args)
+    {
+        int a = 13;
+        int b = 22;
+
+        System.out.println(a + b);
+
+        System.out.println(b - a);
+
+        System.out.println(b * a);
+
+        System.out.println((double)b / (double)a);
+
+        System.out.println(b / a);
+
+        System.out.println(b % a);
+
+
+    }
+}
