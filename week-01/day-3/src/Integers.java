2837ad8217e5b69f297b84a345c2297f5e752445
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 12:52:17 +0100

    5th task

---
+++ week-01/day-3/src/Integers.java
@@ -0,0 +1,15 @@
+public class Integers
+{
+    public static void main(String[] args)
+    {
+        System.out.println(42);
+        System.out.println(-1);
+        System.out.println(0);
+
+        System.out.println("Maximum value:");
+        System.out.println(2147483647);
+
+        System.out.println("Minimum value:");
+        System.out.println(-2147483648);
+    }
+}
