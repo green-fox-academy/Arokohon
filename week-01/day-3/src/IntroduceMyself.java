ef36158cc0a5245a8dadda9b54bf2d05224d18bd
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-29 12:59:57 +0100

    6th task

---
+++ week-01/day-3/src/IntroduceMyself.java
@@ -0,0 +1,9 @@
+public class IntroduceMyself
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, my name is Duffy.");
+        System.out.println("I'm " + 30 + " years old.");
+        System.out.println("I'm " + 1.76 + " m height.");
+    }
+}
