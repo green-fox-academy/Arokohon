4f21f8774d73751039de5bbe17284b60f87f1402
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 21:06:07 +0100

    13th task first try

---
+++ week-01/day-4/src/DiagonalMatrix.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class DiagonalMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+        };
+
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.println(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
