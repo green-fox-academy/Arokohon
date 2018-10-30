1370b1d93a15c88186d656a154e50a845d7f0c32
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 17:30:14 +0100

    11th task

---
+++ week-01/day-4/src/IncrementElement.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class IncrementElement {
+
+    public static void main(String[] args) {
+
+        int[] t = {1, 2, 3, 4, 5};
+        t[2] += 10;
+        System.out.println(t[2]);
+
+    }
+}
