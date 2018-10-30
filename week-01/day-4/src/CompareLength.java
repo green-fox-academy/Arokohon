3dc51379425fdcb3d0f31dc5c9bd22ec421a93c5
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 17:08:53 +0100

    8th task

---
+++ week-01/day-4/src/CompareLength.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class CompareLength {
+
+    public static void main(String[] args) {
+
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5};
+
+        if (p2.length > p1.length){
+
+            System.out.println("p2 has more elements than p1.");
+
+        }
+
+        else{
+
+            System.out.println("p1 has more elements than p2.");
+
+        }
+
+    }
+}
