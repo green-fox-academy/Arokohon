35643f1f40ea634e08ef3940bc2163b0039ff77f
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 17:24:38 +0100

    10th task

---
+++ week-01/day-4/src/ChangeElement.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ChangeElement {
+
+    public static void main(String[] args) {
+
+        int[] s = {1, 2, 3, 8, 5, 6};
+        s[4] = 4;
+
+        System.out.println(s[4]);
+
+    }
+}
