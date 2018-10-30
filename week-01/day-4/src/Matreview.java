239afbc15462ce269124867f961f2015ed59dfe6
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 12:01:34 +0100

    Day4 material review

---
+++ week-01/day-4/src/Matreview.java
@@ -0,0 +1,13 @@
+public class Matreview
+{
+
+    public static void main(String[] args)
+    {
+        int i;
+        for(i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+        System.out.println(i);
+    }
+}
---
+++ week-01/day-4/src/Matreview2.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matreview2 {
+
+    public static void main(String[] args) {
+
+//        int[] myArray = {1,2,3,4,5};
+//        System.out.println(myArray[2]);
+
+//            int[] myArray;
+//            myArray = new int[10];
+//            System.out.println(myArray[3]);
+//
+//            myArray[3] = 2;
+//            System.out.println(myArray[3]);
+
+//        int[] myArray = {1,2,34};
+
+//        for (int i =0; i < myArray.length; i++){
+//            System.out.println(myArray[i]);
+//        }
+
+//        for (int akarMilyenNev : myArray){
+//            System.out.println(akarMilyenNev);
+//        }
+
+//        int a = 2;
+//        int b = 2;
+//        int[] myArray1 = {1,2};
+//        int[] myArray2 = {1,2};
+//
+//        System.out.println(a == b);
+//        System.out.println(myArray1 == myArray2);
+
+//        int a = 2;
+//        int b = 2;
+//        int[] myArray1 = {1,2};
+//        int[] myArray2 = myArray1;
+//        myArray2[0] = 7;
+//
+//        System.out.println(myArray1[0]);
+
+//        int[] myArray = {1,2};
+//        int[][] myMatrix = {{1,2}, {2,1}};
+//        System.out.println(myMatrix[0][0]);
+//        System.out.println(myMatrix[0][1]);
+//        System.out.println(myMatrix[1][0]);
+//        System.out.println(myMatrix[1][1]);
+//        System.out.println(Arrays.toString(myMatrix[0]));
+//        System.out.println(Arrays.toString(myMatrix[1]));
+
+        int myNUmber = 3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a number ");
+        int limit = scanner.nextInt();
+
+        for (int i = 0; i < limit; i++){
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+}
