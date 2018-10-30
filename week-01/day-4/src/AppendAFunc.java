c79d96cc3de98defa7b001f33d868506100c9ab4
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 13:06:51 +0100

    3rd task

---
+++ week-01/day-4/src/AppendAFunc.java
@@ -0,0 +1,15 @@
+public class AppendAFunc {
+
+    public static void main(String[] args) {
+
+        String typo = "Chinchill";
+        System.out.println(appendAFunc(typo));
+
+    }
+
+    public static String appendAFunc(String a){
+
+        return a + "a";
+
+    }
+}
