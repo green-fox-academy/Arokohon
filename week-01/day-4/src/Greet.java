1f8d18050dbeeff773cb478efc67c0d68979e7d6
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 12:54:32 +0100

    2nd task

---
+++ week-01/day-4/src/Greet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Greet {
+
+    public static void main(String[] args) {
+
+        String al = "Greenfox";
+        System.out.println(greet(al));
+    }
+
+    public static String greet(String a){
+
+        return "Greetings dear " + a + "!";
+
+    }
+}
