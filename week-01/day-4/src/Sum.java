b2c695b496ccc9de49077626f817edb3067492d2
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 15:05:42 +0100

    4th task

---
+++ week-01/day-4/src/Sum.java
@@ -0,0 +1,17 @@
+public class Sum {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+    public static int sum (int input){
+
+        int sum =0;
+        for (int i = 0; i < input; i++){
+            sum += i;
+        }
+        return sum;
+
+    }
+}
