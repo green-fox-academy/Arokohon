f4ad1002c00256bbf9e580531b95824eb027b975
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-30 22:04:06 +0100

    13th task with 2 possible solution

--- week-01/day-4/src/DiagonalMatrix.java
+++ week-01/day-4/src/DiagonalMatrix.java
@@ -1,17 +1,52 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class DiagonalMatrix {
 
+//    public static void main(String[] args) {
+//
+//        int[][] matrix = new int [4][4];
+//        fillMatrix(matrix);
+//        printMatrix(matrix);
+//
+//
+//    }
+//
+//    public static void fillMatrix (int[][] matrixToFill){
+//
+//        for (int i = 0; i < matrixToFill.length; i++){
+//            for (int j = 0; j < matrixToFill.length; j++){
+//                if (i == j){
+//                    matrixToFill[i][j] += 1;
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    public static void printMatrix (int[][] matrixToPrint){
+//
+//        for (int i = 0; i < matrixToPrint.length; i++){
+//            for (int j = 0; j < matrixToPrint.length; j++){
+//                System.out.println(" " + matrixToPrint[i][j]);
+//            }
+//            System.out.println();
+//        }
+//    }
+
     public static void main(String[] args) {
-
-        int[][] matrix = {
-        };
-
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix[i].length; j++){
-                System.out.println(matrix[i][j] + " ");
+        Scanner myScanner = new Scanner(System.in);
+        System.out.printf("Matrix Size:");
+        int matrixSize = myScanner.nextInt();
+        int [][] matrix = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i ++){
+            for (int j = 0; j < matrixSize; j ++){
+                if (i == j){
+                    matrix[i][j] = 1;
+                }
             }
-            System.out.println();
+
+            System.out.println(Arrays.toString(matrix[i]));
         }
 
     }
