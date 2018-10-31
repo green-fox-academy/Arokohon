6d48b0284ef42168c0f22ffc9e5ec06c8f879448
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-31 12:26:39 +0100

    16th task

---
+++ week-01/day-3/src/AverageOfInput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class AverageOfInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        System.out.println("Give me 5 numbers!");
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.println("The total values is: " + sumNumbers(numbers) + ".");
+        System.out.println("The average is: " + (sumNumbers(numbers) / numbers.length) + ".");
+
+    }
+
+    public static int sumNumbers (int[] numbers){
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+}
