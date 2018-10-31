05bcf80bcd66ce634648c74e1d87dbb69a4b2c3f
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-10-31 11:02:14 +0100

    14th task

---
+++ week-01/day-3/src/MileToKmConverter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MileToKmConverter {
+
+    public static void main(String[] args) {
+
+        System.out.println("Add a distance in km please!");
+
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+
+        System.out.println("The distance in miles: " + (userInput * 0.62) + ".");
+    }
+}
