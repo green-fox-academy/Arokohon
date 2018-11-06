ee281f3a5e0e34f2e17599ddb9e7a69750d28867
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-06 12:42:25 +0100

    2nd task

--- week-01/day-4/src/DiagonalMatrix.java
+++ week-01/day-4/src/DiagonalMatrix.java
@@ -3,37 +3,6 @@ import java.util.Scanner;
 
 public class DiagonalMatrix {
 
-//    public static void main(String[] args) {
-//
-//        int[][] matrix = new int [4][4];
-//        fillMatrix(matrix);
-//        printMatrix(matrix);
-//
-//
-//    }
-//
-//    public static void fillMatrix (int[][] matrixToFill){
-//
-//        for (int i = 0; i < matrixToFill.length; i++){
-//            for (int j = 0; j < matrixToFill.length; j++){
-//                if (i == j){
-//                    matrixToFill[i][j] += 1;
-//                }
-//            }
-//        }
-//
-//    }
-//
-//    public static void printMatrix (int[][] matrixToPrint){
-//
-//        for (int i = 0; i < matrixToPrint.length; i++){
-//            for (int j = 0; j < matrixToPrint.length; j++){
-//                System.out.println(" " + matrixToPrint[i][j]);
-//            }
-//            System.out.println();
-//        }
-//    }
-
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         System.out.printf("Matrix Size:");
