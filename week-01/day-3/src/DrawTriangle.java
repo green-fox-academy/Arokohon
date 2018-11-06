cf36da83b864ee0b875bb2d24624230f7f69c622
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-06 17:10:41 +0100

    28th task

---
+++ week-01/day-3/src/DrawTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("Type in a whole number!");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        String star = "*";
+        String starInc = "*";
+
+        for (int i = 1; i <= a; i++){
+            System.out.println(star);
+            star = star + starInc;
+            }
+        }
+
+    }
+
