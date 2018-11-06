0d213e9ce5b4584354f536e8bc40bd8d51b89185
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-06 14:20:30 +0100

    4th task

---
+++ week-02/day-1/src/src/TodoPrint.java
@@ -0,0 +1,11 @@
+public class TodoPrint {
+
+    public static void main(String[] args) {
+
+        String toDoText = " - Buy milk\n";
+
+        toDoText = ("My todo:\n") + toDoText + " - Download games\n" + "     - Diablo";
+
+        System.out.println(toDoText);
+    }
+}
