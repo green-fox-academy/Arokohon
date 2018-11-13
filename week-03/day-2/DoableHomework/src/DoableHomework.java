9c7bf88712ec9704b7bc162dadf6d7be9901e441
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-13 12:18:33 +0100

    Doable Homework

---
+++ week-03/day-2/DoableHomework/src/DoableHomework.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class DoableHomework {
+    public static void main(String[] args) {
+        HashMap<String, Object> oak = new HashMap<>();
+        oak.put("type", "European");
+        oak.put("leaf color", "green");
+        oak.put("age", 85);
+        oak.put("sex", "male");
+        HashMap<String, Object> pineTree = new HashMap<>();
+        pineTree.put("type", "American");
+        pineTree.put("leaf color", "green");
+        pineTree.put("age", 115);
+        pineTree.put("sex", "male");
+        HashMap<String, Object> cedrus = new HashMap<>();
+        cedrus.put("type", "Asian");
+        cedrus.put("leaf color", "white");
+        cedrus.put("age", 65);
+        cedrus.put("sex", "female");
+        HashMap<String, Object> acacia = new HashMap<>();
+        acacia.put("type", "European");
+        acacia.put("leaf color", "brown");
+        acacia.put("age", 30);
+        acacia.put("sex", "male");
+        HashMap<String, Object> palm = new HashMap<>();
+        palm.put("type", "Caribbean");
+        palm.put("leaf color", "green");
+        palm.put("age", 70);
+        palm.put("sex", "female");
+    }
+}
