4621a9ff946b253918b17ecc16351213b90ea404
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-14 14:27:13 +0100

    10th task

---
+++ week-03/day-2/TeachersAndStudents/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+  public static String learn;
+
+  public static void learn () {
+    learn = "Learn something";
+  }
+  public void questionTeacher () {
+    Teacher.answer();
+  }
+}
---
+++ week-03/day-2/TeachersAndStudents/src/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+
+  public static String answer;
+
+  public static void answer () {
+    answer = "This is it!";
+  }
+  public void teachStudent () {
+    Student.learn();
+  }
+}
