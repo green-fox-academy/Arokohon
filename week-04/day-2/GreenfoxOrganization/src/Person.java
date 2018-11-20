b73b9b45a2cd694d5255b85bdf40c73b92d4e2b3
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-20 14:39:24 +0100

    1st task - Person class complete

--- week-04/day-2/GreenfoxOrganization/src/Person.java
+++ week-04/day-2/GreenfoxOrganization/src/Person.java
@@ -1,2 +1,22 @@
 public class Person {
+
+  String name;
+  int age;
+  String gender;
+
+  Person (String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  Person person1 = new Person("Jane Doe", 30, "female");
+
+  public static void introduce(String name, int age, String gender) {
+    System.out.println("Hy, I'm " + name + ", " + age + "year old " + gender);
+  }
+
+  public void getGoal() {
+    System.out.println("My goal is: live for the moment!");
+  }
 }
