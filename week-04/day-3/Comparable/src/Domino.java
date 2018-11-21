a4120c39461800df10a0cfaf03581a9198730403
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-11-21 14:23:55 +0100

    2nd task complete - Comparing dominoes

---
+++ week-04/day-3/Comparable/src/Domino.java
@@ -0,0 +1,30 @@
+public class Domino implements Comparable<Domino> {
+  private final int left;
+  private final int right;
+
+  public Domino(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int getLeftSide() {
+    return left;
+  }
+
+  public int getRightSide() {
+    return right;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + left + ", " + right + "]";
+  }
+
+  @Override
+  public int compareTo(Domino o) {
+    if (o.getLeftSide() < this.getLeftSide()) {
+        return 1;
+      }
+      return -1;
+  }
+}
