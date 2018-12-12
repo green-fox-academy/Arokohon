9714a6ea2717fdc943a78509f6a6a734123b34dd
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-12-12 14:37:31 +0100

    Finishing 1st and 2nd tasks - creating basic methods, balance get 2 decimals

--- week-06/day-3/Practice/src/main/java/com/greenfoxacademy/practice/BankAccount.java
+++ week-06/day-3/Practice/src/main/java/com/greenfoxacademy/practice/BankAccount.java
@@ -1,4 +1,48 @@
 package com.greenfoxacademy.practice;
 
 public class BankAccount {
+
+  private String name;
+  private double balance;
+  private String currency;
+  private String animalType;
+
+  public BankAccount(String name, double balance, String currency, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.currency = currency;
+    this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  public void setAnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+
+  public String getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(String currency) {
+    this.currency = currency;
+  }
 }
--- week-06/day-3/Practice/src/main/java/com/greenfoxacademy/practice/BankAccountController.java
+++ week-06/day-3/Practice/src/main/java/com/greenfoxacademy/practice/BankAccountController.java
@@ -1,4 +1,25 @@
 package com.greenfoxacademy.practice;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class BankAccountController {
+
+  private List<BankAccount> bankAccounts = new ArrayList<>();
+
+  public BankAccountController() {
+    bankAccounts.add(new BankAccount("Simba", 2000, "SAFARI", "lion"));
+  }
+
+  @RequestMapping(path = "/show", method = RequestMethod.GET)
+  public String showBankAccounts(Model model) {
+    model.addAttribute("bankAccounts", bankAccounts);
+    return "index";
+  }
 }
