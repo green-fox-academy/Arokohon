47e1ee3f7c1c1e7b2d20f66eab13b902061fad4f
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-12-14 11:48:53 +0100

    Starting the Webshop application - setting up basics

--- week-05/day-3/webshop/src/main/java/com/greenfoxacademy/webshop/WebshopController.java
+++ week-05/day-3/webshop/src/main/java/com/greenfoxacademy/webshop/WebshopController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class WebshopController {
 
-  @GetMapping("/")
-  public String index(Model model) {
-    return null;
+//  @GetMapping("/")
+//  public String index(Model model) {
+//    List<WebshopItem> allItem =this.storage
+//    return "index";
+//  }
+
+  @GetMapping("/add-product")
+  public String addProduct() {
+    return "add-product";
   }
 }
