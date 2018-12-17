657f2dafd50e7426e50ce18af7e44958837fe7e9
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-12-17 16:18:19 +0100

    Creating basics - adding CommandLineRunner

--- week-07/day-1/ConnectionWithMySQL/src/main/java/com/greenfox/connectionwithmysql/ConnectionwithmysqlApplication.java
+++ week-07/day-1/ConnectionWithMySQL/src/main/java/com/greenfox/connectionwithmysql/ConnectionwithmysqlApplication.java
@@ -1,14 +1,21 @@
 package com.greenfox.connectionwithmysql;
 
+import com.greenfox.connectionwithmysql.controllers.TodoController;
+import com.greenfox.connectionwithmysql.models.ToDo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConnectionwithmysqlApplication {
+public class ConnectionwithmysqlApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(ConnectionwithmysqlApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    TodoController.todorepo.save(new ToDo("Just go on!"));
+  }
 }
 
