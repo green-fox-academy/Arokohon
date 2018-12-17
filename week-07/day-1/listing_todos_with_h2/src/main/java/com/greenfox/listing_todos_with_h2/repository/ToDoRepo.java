7279242112deb129b10fd175275b56ff983b3b9e
Szőke Tamás <tamas.szoke.88@gmail.com>
2018-12-17 12:34:47 +0100

    Starting 1st task - creating packages and basics

--- week-07/day-1/listing_todos_with_h2/src/main/java/com/greenfox/listing_todos_with_h2/repository/ToDoRepo.java
+++ week-07/day-1/listing_todos_with_h2/src/main/java/com/greenfox/listing_todos_with_h2/repository/ToDoRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ToDoInterface extends CrudRepository<ToDo, Integer> {
+public interface ToDoRepo extends CrudRepository<ToDo, Integer> {
 }
