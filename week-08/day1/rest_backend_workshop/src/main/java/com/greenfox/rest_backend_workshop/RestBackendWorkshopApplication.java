7803ba5dc2e6126ee092f17ff7394dd5712adc27
Szőke Tamás <tamas.szoke.88@gmail.com>
2019-01-07 14:32:44 +0100

    Starting workshop, creating basics

---
+++ week-08/day1/rest_backend_workshop/src/main/java/com/greenfox/rest_backend_workshop/RestBackendWorkshopApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.rest_backend_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBackendWorkshopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestBackendWorkshopApplication.class, args);
+  }
+
+}
+
---
+++ week-08/day1/rest_backend_workshop/src/test/java/com/greenfox/rest_backend_workshop/RestBackendWorkshopApplicationTests.java
@@ -0,0 +1,17 @@
+package com.greenfox.rest_backend_workshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestBackendWorkshopApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
