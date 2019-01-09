1d0f57be602a4c893375c6bc6b70b3a040150db7
Szőke Tamás <tamas.szoke.88@gmail.com>
2019-01-09 11:17:12 +0100

    Starting aliasapp, creating basics

---
+++ week-08/day-3/aliasapp/src/main/java/com/greenfox/aliasapp/AliasappApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.aliasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliasappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AliasappApplication.class, args);
+  }
+
+}
+
---
+++ week-08/day-3/aliasapp/src/test/java/com/greenfox/aliasapp/AliasappApplicationTests.java
@@ -0,0 +1,17 @@
+package com.greenfox.aliasapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AliasappApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
