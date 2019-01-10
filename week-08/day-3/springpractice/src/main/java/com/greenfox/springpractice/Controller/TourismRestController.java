cec17eaee99ce2598234a5de8843a93489c7f3dd
Szőke Tamás <tamas.szoke.88@gmail.com>
2019-01-10 13:50:28 +0100

    Practice Spring project, creating basics

---
+++ week-08/day-3/springpractice/src/main/java/com/greenfox/springpractice/Controller/TourismRestController.java
@@ -0,0 +1,4 @@
+package com.greenfox.springpractice.Controller;
+
+public class TourismRestController {
+}
---
+++ week-08/day-3/springpractice/src/main/java/com/greenfox/springpractice/SpringpracticeApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringpracticeApplication.class, args);
+  }
+
+}
+
